2dc32729e1ad19db4a2bab39a0537cf7fd7f7930
Guillermo Fernández <gfernandez1985@gmail.com>
2011-11-08 20:58:08 -0200

    modificado hello-gconf para que utilise/defina una variable tipo int

--- ejemplos/gconf/hello-gconf.c
+++ ejemplos/gconf/hello-gconf.c
@@ -8,20 +8,18 @@ char *monitor = "/apps/mousecam";
 
 void setKeys(GConfClient *client, guint cnxn_id, GConfEntry *entry,
 		gpointer user_data) {
-	printf("callback, nuevo valor: %s\n", gconf_value_get_string(gconf_entry_get_value(entry)));
-	gconf_client_get_string(client, key, NULL);
+	printf("callback, nuevo valor: %d\n", gconf_value_get_int(gconf_entry_get_value(entry)));
+	gconf_client_get_int(client, key, NULL);
 }
 
 int main(int argc, char* argv[]) {
 	GConfClient *client;
 
-	//char *key = "/apps/mousecam/MyVal";
-	//char *monitor = "/apps/mousecam";
 	char *valor;
+    int intValor;
 	int clientId;
 
 	client = gconf_client_get_default();
-//	gconf_client_set_string(client, key, "COSA", NULL);
 	gconf_client_add_dir(client, monitor, GCONF_CLIENT_PRELOAD_NONE, NULL);
 
 	/* Connect the check_button to a callback that can toggle it when the option
@@ -34,17 +32,16 @@ int main(int argc, char* argv[]) {
 	                          NULL,
 	                          NULL);
 
-	valor = gconf_client_get_string(client, key, NULL);
-	if (valor == NULL) {
-		printf("valor es null\n");
-		return 1;
-	}
-	printf("%s\n", valor);
+    //valor = gconf_client_get_string(client, key, NULL);
+	intValor = gconf_client_get_int(client, key, NULL);
+    
+
 	if (argc > 1) {
-		gconf_client_set_string(client, key, argv[1], NULL);
-		printf("Cambiado el fondo por %s\n", argv[1]);
+		gconf_client_set_int(client, key, atoi(argv[1]), NULL);
+		printf("Seteado valor %s recibido por parametro.\n", argv[1]);
 	} else {
-		printf("No me mandaste a cambiar nada\n");
+        gconf_client_set_int(client, key, 1000, NULL);
+		printf("Seteado valor por defecto 1000\n");
 	}
 
 	gtk_main();
